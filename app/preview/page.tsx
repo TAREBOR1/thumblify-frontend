64e9dd102d65f6ac022b8b0f94e250b2e87bee18
tarebor <onodaifetareborgift@gmail.com>
2026-01-14 20:44:42 +0100

    added a new component and fixed suspense error

--- app/preview/page.tsx
+++ app/preview/page.tsx
@@ -1,21 +1,12 @@
-"use client"
 
-import { yt_html } from "@/assets/assets";
-import { useSearchParams } from "next/navigation";
+import PreviewContent from "@/components/PreviewContent";
+import { Suspense } from "react";
 
 const Page = () => {
 
-    const params =useSearchParams()
-    const thumbnail_url=params.get('thumbnail_url')
-    const title=params.get('title')
-
-    const new_html= yt_html.replace('%%THUMBNAIL_URL%%',thumbnail_url!).replace("%%TITLE%%",title!)
-   
-    return ( <>
-    <div className="fixed inset-0 z-100 bg-black">
-          <iframe srcDoc={new_html} width="100%" height="100%" allowFullScreen></iframe>
-    </div>
-    </> );
+   <Suspense fallback={<div className="text-white">Loading Preview...</div>}>
+    <PreviewContent/>
+   </Suspense>
 }
  
 export default Page;
